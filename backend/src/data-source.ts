afba0beccfd57cb5eca0be51a0ca9d6200954900
Yue-Temple <ssterape@gmail.com>
2025-02-15 02:41:32 +0900

    DB

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -9,14 +9,11 @@ import { RefreshToken } from './entity/RefreshToken';
 
 export const AppDataSource = new DataSource({
   type: 'postgres', // データベースタイプはpostgresに設定
-  host: 'localhost', // ホスト名（localhostに接続する場合）
-  port: 5432, // PostgreSQLのデフォルトポート
-  username: 'postgres', // PostgreSQLのユーザー名
-  password: '7979', // パスワード
-  database: 'tkpro_database', // 使用するデータベース名
+  url: process.env.DATABASE_URL, // 環境変数で設定したURLを使用
   synchronize: true, // 本番環境では false にすることを推奨
   logging: false,
   entities: [User, Menu, Profile, History, HistoryItem, RefreshToken], // 必要なエンティティをすべてインポートして追加
   migrations: [],
   subscribers: [],
 });
+
